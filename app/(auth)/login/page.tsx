85ba53728e99c2e9226ce4b4967034e83ec9f804
Daniel Rubin <daniel.rubin1997@gmail.com>
2024-03-22 17:57:48 +0100

    feat: worked on configurting project

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex w-full flex-col items-center self-center bg-background p-4">
+      <h1 className="text-3xl">Login</h1>
+    </div>
+  );
+}
