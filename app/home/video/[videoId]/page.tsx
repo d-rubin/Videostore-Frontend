46ad28ac2fafe483c3acdae5553afd5628695cbc
Daniel Rubin <daniel.rubin1997@gmail.com>
2024-05-23 17:30:00 +0200

    feat: get video.js to work

--- app/home/video/[videoId]/page.tsx
+++ app/home/video/[videoId]/page.tsx
@@ -1,6 +1,7 @@
 import { getSingleVideoUrl } from "@/lib/helper";
 import { ResolutionSelect } from "@/components/ResolutionSelect";
 import { TResolution } from "@/lib/actions";
+import VideoPlayer from "@/components/VideoPlayer";
 
 export default async function VideoPage({
   params,
@@ -10,16 +11,10 @@ export default async function VideoPage({
   searchParams?: { resolution?: TResolution };
 }) {
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col items-end">
-        <ResolutionSelect className="mr-2" />
-        <video controls autoPlay className="">
-          <source
-            src={getSingleVideoUrl(params.videoId, searchParams?.resolution)}
-            type="application/x-mpegURL"
-          />
-        </video>
-      </div>
+    <div className="mx-auto max-h-dvh max-w-screen-2xl">
+      <VideoPlayer
+        src={getSingleVideoUrl(params.videoId, searchParams?.resolution)}
+      />
     </div>
   );
 }
